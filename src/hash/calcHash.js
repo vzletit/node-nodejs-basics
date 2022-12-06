d84835ac4b3ac0c976b4bcc01c9eba52bd8519aa
Dmitry Agurkov <munnharpa@gmail.com>
2022-12-06 16:38:24 +0300

    Задание в hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import crypto from 'crypto';
+import { promises as fs } from "fs";
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join('files', 'fileToCalculateHashFor.txt');
+    const salt = '123';
+    const hasher = crypto.createHmac('sha256', salt);
+    
+    const data = await fs.readFile(filePath, { encoding: 'utf-8' }, (err) => console.log(err || null));
+        
+    const hash = hasher.update(data).digest("hex");
+    console.log(hash)
 };
 
 await calculateHash();
