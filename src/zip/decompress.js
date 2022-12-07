d03ef88ee646e8481ee7315c57c1a8bc84537a5d
Dmitry Agurkov <munnharpa@gmail.com>
2022-12-07 16:02:23 +0300

    zip finished

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inputFilePath = path.join(__dirname, 'files', 'archive.gz');
+const outputFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(inputFilePath);
+    const writeStream = fs.createWriteStream(outputFilePath);
+    const deCompressStream = zlib.createGunzip();
+
+    readStream
+        .pipe(deCompressStream)
+        .pipe(writeStream)
 };
 
 await decompress();
